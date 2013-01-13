364014534a54efadaccdf30c2bd96e942703e108
Michael Cotterell <mepcotterell@gmail.com>
2013-01-13 14:55:28 -0500

    added stuff

---
+++ src/main/java/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+	
+	public static void main(String[] args) {
+
+		Life game = new Life();
+
+	} // main
+	
+} // Driver
---
+++ src/main/java/Life.java
@@ -0,0 +1,43 @@
+
+
+public class Life {
+
+    private boolean running;
+    private boolean[][] grid;
+
+    /**
+     *
+     */
+	public Life() {
+
+	} // Life
+
+	/**
+	 *
+	 */
+	public void display() {
+
+	} // display
+
+	/**
+	 *
+	 */
+	public void prompt() {
+
+	} // prompt
+
+	/**
+	 *
+	 */
+	public void update() {
+
+	} // update
+
+	/**
+	 *
+	 */
+	public boolean isRunning() {
+
+	} // isRunning
+
+} // Life
