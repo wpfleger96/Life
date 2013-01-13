3eb822fe630916ad488661d50628e397f793a775
Michael Cotterell <mepcotterell@gmail.com>
2013-01-13 15:12:26 -0500

    updated the driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,7 +2,7 @@ public class Driver {
 	
 	public static void main(String[] args) {
 
-		Life game = new Life("https://raw.github.com/mepcotterell-cs1302/cs1302-life/master/seeds/seed1.txt");
+		Life game = new Life("seeds/seed1.txt");
 
 		// game loop
 		while (game.isRunning()) {
